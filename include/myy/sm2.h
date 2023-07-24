941eaa0189a22542f19c8b44bc4971266e9793cb
myy5981 <myy5981@outlook.com>
2023-07-24 21:51:28 +0800

    sm2-4

--- include/myy/sm2.h
+++ include/myy/sm2.h
@@ -6,19 +6,16 @@
 
 __CPP_BEGIN
 
-struct _SM2_JPOINT {
+typedef struct _SM2_JPOINT {
 	BN_256 x;
 	BN_256 y;
 	BN_256 z;
-};
+} SM2_JPOINT;
 
-struct _SM2_POINT {
+typedef struct _SM2_POINT {
 	BN_256 x;
 	BN_256 y;
-};
-
-typedef struct _SM2_JPOINT	SM2_JPOINT;
-typedef struct _SM2_POINT	SM2_POINT;
+} SM2_POINT;
 
 #define	sm2_jpoint_is_zero(p)	bn_256_is_zero((p)->z)
 #define	sm2_jpoint_cpy(r,a)		bn_256_cpy((r)->x,(a)->x);bn_256_cpy((r)->y,(a)->y);bn_256_cpy((r)->z,(a)->z)
@@ -29,12 +26,20 @@ extern	void	sm2_jpoint_dbl			(SM2_JPOINT* r, const SM2_JPOINT* a);
 extern	void	sm2_jpoint_add_point	(SM2_JPOINT* r, const SM2_JPOINT* a, const SM2_POINT* b);
 extern	void	sm2_point_mul			(SM2_JPOINT* r, const SM2_POINT* a, const BN_256 k);
 
+/**
+ * 基于蒙哥马利的点运算，效率比起上面的稍快一点，所有参与运算的点均需先转换为蒙哥马利域表示
+*/
+
 #define	sm2_jpoint_is_zero_mont(p)			bn_256_GFp_is_zero_mont((p)->z)
 extern	void	sm2_jpoint_to_point_mont	(SM2_POINT* r, const SM2_JPOINT* a);
 extern	void	sm2_point_to_jpoint_mont	(SM2_JPOINT* r, const SM2_POINT* a);
 extern	void	sm2_jpoint_dbl_mont			(SM2_JPOINT* r, const SM2_JPOINT* a);
 extern	void	sm2_jpoint_add_point_mont	(SM2_JPOINT* r, const SM2_JPOINT* a, const SM2_POINT* b);
 extern	void	sm2_point_mul_mont			(SM2_JPOINT* r, const SM2_POINT* a, const BN_256 k);
+
+/**
+ * 将点转到蒙哥马利域的表示或从蒙哥马利域表示还原
+*/
 
 extern	void	sm2_jpoint_to_mont			(SM2_JPOINT* a);
 extern	void	sm2_jpoint_from_mont		(SM2_JPOINT* a);
