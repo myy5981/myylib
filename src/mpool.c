ede828a0ff6bc9f431e0d2445f618098d392c8e3
myy5981 <myy5981@outlook.com>
2025-03-30 21:44:38 +0800

    mpool-3

--- src/mpool.c
+++ src/mpool.c
@@ -141,12 +141,14 @@ void mpool_free_1024b(MPOOL_1024B* pool, void* ptr) {
 
 void destory_mpool_1024b(MPOOL_1024B* pool) {
 	if (pool != NULL) {
-		MCHUNK_1024B* chunk = &(pool->root);
+		MCHUNK_1024B* chunk = &(pool->root.next_chunk);
 		while (chunk != NULL) {
 			MCHUNK_1024B* next = chunk->next_chunk;
 			mchunk_free_1024b(chunk);
 			chunk = next;
 		}
+		free(pool->root.free);
+		munmap(pool->root.m, pool->root.capacity * 1024);
 		free(pool);
 	}
 }
